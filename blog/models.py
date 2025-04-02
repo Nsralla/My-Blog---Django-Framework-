e264e2147470b247d38bc89ebd06070a4093a179
Nsralla <nsrhassan22@gmail.com>
2025-04-02 13:25:13 +0300

    creating models for post, author, and tag

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 # Create your models here.
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.caption
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.email_address}"
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    excerpt = models.CharField(max_length=200)
+    content= models.TextField(validators=[MinLengthValidator(10), MaxLengthValidator(10000)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL,null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author} on {self.date}  "
+    
+    def get_content(self):
+        return self.content[:100] + "..." if len(self.content) > 100 else self.content
